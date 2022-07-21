d808fc5c7b24bb5bafe5074fadeec29a1cf7c0da
Seddik-Abbassi <abbassi.seddik@gmail.com>
2022-07-21 11:49:55 +0100

    first commit

---
+++ src/main/java/com/seddik/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.seddik.springjenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+    @PostConstruct
+    public void intt() {
+        logger.info("Application started...");
+    }
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(SpringJenkinsApplication.class , args);
+    }
+
+}
